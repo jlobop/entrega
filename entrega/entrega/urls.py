166a20ba8ef9e23c677ca515429eb2e5495b91d7
jlobo <jlobo@192.168.1.8>
2017-11-13 00:01:50 -0300

    error de sintax

--- entrega/entrega/urls.py
+++ entrega/entrega/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^biblio/', include('biblio.urls'))
+    url(r'^biblio/', include('biblio.urls')),
     url(r'^admin/', admin.site.urls),
-]
+    ]
