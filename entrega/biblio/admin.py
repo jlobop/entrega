17a6aa72ae46452ad4fdf65b5ae6592ab9797ca7
jlobo <jlobo@192.168.1.8>
2017-11-12 23:58:38 -0300

    +Urls

--- entrega/biblio/admin.py
+++ entrega/biblio/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Libro
+admin.site.register(Libro)
+
+from .models import Prestamo
+admin.site.register(Prestamo)
+
+from .models import Copia
+admin.site.register(Copia)
+
+from .models import Socio
+admin.site.register(Socio)
