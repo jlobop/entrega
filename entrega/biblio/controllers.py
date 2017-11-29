a564678f04982d48b70d3feaf28f177a24c466bf
juartinv <juartinv@gmail.com>
2017-11-29 18:06:23 -0300

    Version Alfa

--- entrega/biblio/controllers.py
+++ entrega/biblio/controllers.py
@@ -7,32 +7,21 @@ from django.db import models
 class GestorPrestamos(object):
 
     def getPrestados(**kwargs):
-        #print(kwargs)
         return (Prestamo.objects.filter(**kwargs))
-
-    def __str__(self):
-        return (str(self.Isbn)+", "+self.Titulo)
-    #def was_published_recently(self):
-     #   return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
 
     def devolver(Id_socio, Inventario):
         try:
-            #prestamo = GestorPrestamos.getPrestados(Id=Id_socio, Inventario=Inventario)
-            prestamo = Prestamo.objects.get(Id=Id_socio, Inventario=Inventario, Estado = 'Pendiente')
+            prestamo = Prestamo.objects.get(Id_socio=Id_socio, Inventario=Inventario, Estado = 'Pendiente')
 
         except Exception as e:
-            #print(type(e))
-            #if str(type(e)) == 'biblio.models.DoesNotExist':
             return "No existe el prestamo solicitado"
-            #return "Fallo algo"
 
         Fecha_devolucion = prestamo.Calcular_Fecha_devolucion()
         print(type(time.strftime("%Y-%m-%d")))
 
         quedomoroso=False
-        #if Fecha_devolucion < time.strftime("%Y-%m-%d") + datetime.timedelta(days=7):
         if Fecha_devolucion < datetime.date.today():
-            socio = prestamo.Id
+            socio = prestamo.Id_socio
             socio.Estado_moroso = True
             quedomoroso = True
             socio.save()
@@ -44,19 +33,14 @@ class GestorPrestamos(object):
         prestamo.Estado = 'Terminado'
         prestamo.save()
 
-        msg = "Muchas Gracias por devolver el libro!" if not quedomoroso else 'El libro se devolvio tarde, el socio quedo moroso.'
+        msg = "Muchas Gracias por devolver el libro! " if not quedomoroso else 'El libro se devolvio tarde, el socio quedo moroso.'
+        msg = msg + " <br><a href='/biblio/'>Volver</a>"
         return msg
+        #return prestamo
+    
+    def __str__(self):
+        return (str(self.Isbn)+", "+self.Titulo)
 
-        '''
-        if type(prestamo.Fecha_prestamo) == str:
-            fechap = datetime.datetime.strptime(self.Fecha_prestamo, '%Y-%m-%d')
-        else:
-            fechap = self.Fecha_prestamo
-        Fecha_devolucion = fechap + timedelta(days=7)
-        if type(Fecha_devolucion) == datetime.datetime:
-            Fecha_devolucion = datetime.datetime.date(Fecha_devolucion)
-        '''
-        return prestamo
 
 
 class GestorSocios(object):
@@ -66,6 +50,7 @@ class GestorSocios(object):
 
     def get(**kwargs):
         return (Socio.objects.get(**kwargs))
+
 
 class GestorCopias(object):
 
--- entrega/biblio/forms.py
+++ entrega/biblio/forms.py
@@ -1,13 +1,65 @@
 from django import forms
 from .models import *
 
+'''
 class SocioForm(forms.ModelForm):
     class Meta:
         model=Socio
-        #fields=('Apellido','Id','Nombre')
+        #fields=('Id_socio',)
         fields='__all__'
 
 class LibroForm(forms.ModelForm):
     class Meta:
         model=Libro
         fields=('Isbn',)
+'''
+
+class SocioForm(forms.Form):
+    #Id_socio = forms.IntegerField()
+    Id_socio = forms.ModelMultipleChoiceField(queryset=Socio.objects.all())
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='info_socio')
+
+class LibroForm(forms.Form):
+    Isbn = forms.ModelMultipleChoiceField(queryset=Libro.objects.all())
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='info_libro')
+
+
+class AltaSocioForm(forms.Form):
+    #Id_socio = forms.IntegerField()
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='alta_socio')   
+    Nombre = forms.CharField(max_length=20)
+    Apellido = forms.CharField(max_length=20)
+    Email = forms.EmailField()
+    Fecha_nac = forms.DateField(widget=forms.widgets.DateInput(format="%Y-%m-%d"),label='Fecha de Nacimiento')
+
+class AltaLibroForm(forms.Form):   
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='alta_libro')   
+    Isbn = forms.IntegerField(min_value=1000000000000,max_value=9999999999999)
+    Titulo = forms.CharField(max_length=60)
+    Autor = forms.CharField(max_length=60)
+    Fecha_ingreso = forms.DateField(widget=forms.widgets.DateInput(format="%Y-%m-%d"),initial=datetime.date.today())
+    ncopias = forms.IntegerField(min_value=1,max_value=10,initial=1,label='Cantidad de Copias')
+
+class CopiaForm(forms.Form):
+    Inventario = forms.ModelMultipleChoiceField(queryset=Copia.objects.all())
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='info_copia')
+    
+class MorososForm(forms.Form):
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='morosos')
+
+class Futuros_morososForm(forms.Form):
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='futuros_morosos')
+
+class Prestamo_fechaForm(forms.Form):
+    Fecha=forms.DateField(widget=forms.widgets.DateInput(format="%Y-%m-%d"))
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='prestamo_fecha')
+    
+class PrestamoForm(forms.Form):
+    Id_socio = forms.ModelMultipleChoiceField(queryset=Socio.objects.filter(Estado_moroso=False))
+    Isbn = forms.ModelMultipleChoiceField(queryset=Libro.objects.all())
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='prestamo')
+    
+class DevolucionForm(forms.Form):
+    Id_socio = forms.ModelMultipleChoiceField(queryset=Socio.objects.all())
+    Inventario = forms.ModelMultipleChoiceField(queryset=Copia.objects.filter(Prestado=True))
+    solicitud = forms.CharField( widget=forms.HiddenInput(),initial='devolucion')
